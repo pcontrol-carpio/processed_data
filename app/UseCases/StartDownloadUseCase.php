67db8841a954e0b9b7d498768eeea6e7a19e7d93
Marcus Brasizza <marcus.brasizza@epoc.tech>
2025-06-17 15:51:53 -0300

    env

--- app/UseCases/StartDownloadUseCase.php
+++ app/UseCases/StartDownloadUseCase.php
@@ -6,15 +6,51 @@ use DB;
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use ZipArchive;
 
 class StartDownloadUseCase
 {
 
+    private function fileCorrupted($file)
+    {
+        $zip = new ZipArchive();
+        if ($zip->open($file) === true) {
+            if ($zip->numFiles > 0) {
+
+                $zip->close();
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
     private function download($file)
     {
 
-        $zipPath = tempnam(sys_get_temp_dir(), 'zip_');
+        $name = basename($file);
 
+        $zipPath = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $name;
+        if(file_exists($zipPath)) {
+            echo "Arquivo já baixado: $zipPath\n";
+            // Verifica se o arquivo está corrompido
+            // Se estiver corrompido, remove o arquivo
+            // e baixa novamente
+            // Se não estiver corrompido, retorna o caminho do arquivo
+            // e não baixa novamente
+            echo "Verificando integridade do arquivo...\n";
+            echo "Arquivo: $zipPath\n";
+            echo "Tamanho do arquivo: " . filesize($zipPath) . " bytes\n";
+            if ($this->fileCorrupted($zipPath)) {
+                echo "Arquivo corrompido, removendo e baixando novamente...\n";
+                unlink($zipPath);
+            } else {
+                echo "Arquivo não corrompido, retornando...\n";
+                return $this->returnFile($zipPath);
+            }
+        }
         $client = new Client();
         try {
             $client->request('GET', $file, [
@@ -73,7 +109,6 @@ class StartDownloadUseCase
             ->where('arquivo', $file)
 
             ->first();
-
 
         if ($incompleto == null || empty($incompleto->concluido_em)) {
             if ($incompleto == null) {
