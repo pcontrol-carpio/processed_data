61eee8e26ce809fcf497ca08e523052077274dff
Marcus Brasizza <marcus.brasizza@epoc.tech>
2025-06-18 15:41:34 -0300

    env

--- app/UseCases/SocioUseCase.php
+++ app/UseCases/SocioUseCase.php
@@ -30,18 +30,23 @@ class SocioUseCase extends CsvChunkReader
         $startChunk = $progress->last_chunk ?? 0;
         // Lê o arquivo CSV em chunks, processando cada chunk
         foreach ($this->readCsv($file, $this->colunas, $startChunk) as $chunk) {
-            // Visualização: Mostra cada linha que será inserida/atualizada
-            foreach ($chunk as $linha) {
-                echo 'Upserting: ' . json_encode($linha, JSON_UNESCAPED_UNICODE) . PHP_EOL;
-            }
-            // Tenta inserir ou atualizar os dados na tabela 'socio'
-            // Se ocorrer um erro, ele será capturado e registrado no arquivo de log
             try{
             DB::table('socio')->upsert($chunk, ['cnpj_basico'], $this->colunas);
                 echo '✅ OK - Chunk com ' . count($chunk) . ' registros inserido.' . PHP_EOL;
             }catch(Exception $e){
-                    file_put_contents('/tmp/erro.txt', print_r($e->getMessage(),true).PHP_EOL.print_r($chunk,true));
-                    dd($e);
+                   echo '❌ Erro ao processar chunk: ' . $e->getMessage() . PHP_EOL;
+                echo "Testando a linha que deu erro" . PHP_EOL;
+                foreach ($chunk as $key => $linha) {
+                    try {
+                        echo 'Linha: ' . json_encode($linha, JSON_UNESCAPED_UNICODE) . PHP_EOL;
+                        DB::table('socio')->upsert([$linha], ['cnpj_basico'], $this->colunas);
+                    } catch (Exception $e) {
+                        echo '❌ Erro ao inserir linha: ' . $e->getMessage() . PHP_EOL;
+                        exit;
+                    }
+                    file_put_contents('/tmp/erro.txt', print_r($e->getMessage(), true) . PHP_EOL . print_r($chunk, true));
+                    continue; // Continua para o próximo chunk
+                }
             }
         }
 
