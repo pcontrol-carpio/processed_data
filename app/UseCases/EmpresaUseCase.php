5f1a6147247089d562fecc8fc601bc78206566c9
Marcus Brasizza <marcus.brasizza@epoc.tech>
2025-06-18 09:23:03 -0300

    env

--- app/UseCases/EmpresaUseCase.php
+++ app/UseCases/EmpresaUseCase.php
@@ -18,7 +18,7 @@ class EmpresaUseCase extends CsvChunkReader
     ];
     public function __invoke($file)
     {
-        $progress = DB::table('csv_progress')->where('filename', basename($file))->first();
+        $progress   = DB::table('csv_progress')->where('filename', basename($file))->first();
         $startChunk = $progress->last_chunk ?? 0;
         foreach ($this->readCsv($file, $this->colunas, $startChunk) as $chunk) {
             try {
@@ -41,33 +41,33 @@ class EmpresaUseCase extends CsvChunkReader
 
                     }
                 }
-                 $inicio = microtime(true);
+                $inicio = microtime(true);
                 DB::table('empresa')->upsert($chunk, ['cnpj_basico'], $this->colunas);
-                $fim = microtime(true);
+                $fim   = microtime(true);
                 $tempo = $fim - $inicio;
-                echo '✅ OK - Chunk com ' . count($chunk) . ' em ' .basename($file). ' registros inserido.' . number_format($tempo, 2) . ' segundos.' . PHP_EOL;
-
+                echo '✅ OK - Chunk com ' . count($chunk) . ' em ' . basename($file) . ' registros inserido.' . number_format($tempo, 2) . ' segundos.' . PHP_EOL;
 
             } catch (Exception $e) {
-               echo '❌ Erro ao processar chunk: ' . $e->getMessage() . PHP_EOL;
-               echo "Testando a linha que deu erro" . PHP_EOL;
-               foreach ($chunk as $key => $linha) {
-                try{
-                    echo 'Linha: ' . json_encode($linha, JSON_UNESCAPED_UNICODE) . PHP_EOL;
-                    DB::table('empresa')->upsert([$linha], ['cnpj_basico'], $this->colunas);
-                } catch (Exception $e) {
-                    echo '❌ Erro ao inserir linha: ' . $e->getMessage() . PHP_EOL;
-                    exit;
+                echo '❌ Erro ao processar chunk: ' . $e->getMessage() . PHP_EOL;
+                echo "Testando a linha que deu erro" . PHP_EOL;
+                foreach ($chunk as $key => $linha) {
+                    try {
+                        echo 'Linha: ' . json_encode($linha, JSON_UNESCAPED_UNICODE) . PHP_EOL;
+                        DB::table('empresa')->upsert([$linha], ['cnpj_basico'], $this->colunas);
+                    } catch (Exception $e) {
+                        echo '❌ Erro ao inserir linha: ' . $e->getMessage() . PHP_EOL;
+                        exit;
+                    }
+                    file_put_contents('/tmp/erro.txt', print_r($e->getMessage(), true) . PHP_EOL . print_r($chunk, true));
+                    continue; // Continua para o próximo chunk
                 }
-                file_put_contents('/tmp/erro.txt', print_r($e->getMessage(), true) . PHP_EOL . print_r($chunk, true));
-                continue; // Continua para o próximo chunk
             }
-        }
             echo '✅ Todos os registros foram processados com sucesso.' . PHP_EOL;
-        // Retorna true para indicar que o processamento foi concluído com sucesso
-        echo '✅ Processamento concluído.' . PHP_EOL;
-        return true;
+            // Retorna true para indicar que o processamento foi concluído com sucesso
+            echo '✅ Processamento concluído.' . PHP_EOL;
+            return true;
 
+        }
     }
 
 }
