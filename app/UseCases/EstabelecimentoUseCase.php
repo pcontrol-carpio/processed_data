86bafff8944c2c0128421377dcc6a3f31f499165
Marcus Brasizza <marcus.brasizza@epoc.tech>
2025-06-20 16:06:35 -0300

    env

--- app/UseCases/EstabelecimentoUseCase.php
+++ app/UseCases/EstabelecimentoUseCase.php
@@ -39,12 +39,12 @@ class EstabelecimentoUseCase extends CsvChunkReader
         'situacao_especial',
         'data_situacao_especial',
     ];
-   private function formatarData($data)
+    private function formatarData($data)
     {
         if ($data == null || $data == '') {
             return null;
         }
-        return (int)substr($data, 0, 4);
+        return (int) substr($data, 0, 4);
     }
     private function pegarEmpresa($baseCnpj)
     {
@@ -53,52 +53,49 @@ class EstabelecimentoUseCase extends CsvChunkReader
                 ->first())->toArray();
     }
 
-
-     private function pegarSimples($baseCnpj)
+    private function pegarSimples($baseCnpj)
     {
         return collect(DB::table('simples')
                 ->where('cnpj_basico', $baseCnpj)
                 ->first())->toArray();
     }
 
-
-
-    private function popularTabelaBase($idEstabelecimento, $empresa,  $simples , $linha)
+    private function popularTabelaBase($idEstabelecimento, $empresa, $simples, $linha)
     {
-       $capital_social = (float)$empresa['capital_social'];
-        $natureza_juridica = (int)$empresa['natureza_juridica'];
-        $porte = (int)$empresa['porte'];
-        $municipio = (int)$linha['municipio'];
-        $matriz_filial = (int)$linha['matriz_filial'];
-        $situacao_cadastral = (int)$linha['situacao_cadastral'];
-        $motivo_situacao_cadastral = (int)$linha['motivo_situacao_cadastral'];
-        $dados = [
-            'estabelecimento_id' => $idEstabelecimento,
-            //empresa
-            'razao_social' => $empresa['razao_social'], //ok
-            'natureza_juridica' => $natureza_juridica,
-            'capital_social' => $capital_social,
-            'porte' => $porte,
-            //estabelecimento
-            'empresa_id' => $linha['empresa_id'], //ok
-            'cnpj' => $linha['cnpj'], //ok
-            'nome_fantasia' => $linha['nome_fantasia'], //ok
-            'cnae_fiscal_principal' => $linha['cnae_fiscal_principal'], //ok
-            'uf' => $linha['uf'], //ok
-            'municipio' => $municipio,
-            'bairro' => $linha['bairro'], //ok
-            'data_inicio_atividade' => $this->formatarData($linha['data_inicio_atividade']), //ok
-            'matriz' => $matriz_filial,
-            'simples' =>( $simples['opcao_pelo_simples'] == 'S')? 1 : 0, //ok
-            'mei' => ($simples['opcao_pelo_mei'] == 'S')? 1 : 0, //ok
-            'situacao_cadastral' => $situacao_cadastral,
-            'data_situacao_cadastral' => $this->formatarData($linha['data_situacao_cadastral']), //ok
+        $capital_social            = (float) $empresa['capital_social'];
+        $natureza_juridica         = (int) $empresa['natureza_juridica'];
+        $porte                     = (int) $empresa['porte'];
+        $municipio                 = (int) $linha['municipio'];
+        $matriz_filial             = (int) $linha['matriz_filial'];
+        $situacao_cadastral        = (int) $linha['situacao_cadastral'];
+        $motivo_situacao_cadastral = (int) $linha['motivo_situacao_cadastral'];
+        $dados                     = [
+            'estabelecimento_id'        => $idEstabelecimento,
+                                                                     //empresa
+            'razao_social'              => $empresa['razao_social'], //ok
+            'natureza_juridica'         => $natureza_juridica,
+            'capital_social'            => $capital_social,
+            'porte'                     => $porte,
+                                                                            //estabelecimento
+            'empresa_id'                => $linha['empresa_id'],            //ok
+            'cnpj'                      => $linha['cnpj'],                  //ok
+            'nome_fantasia'             => $linha['nome_fantasia'],         //ok
+            'cnae_fiscal_principal'     => $linha['cnae_fiscal_principal'], //ok
+            'uf'                        => $linha['uf'],                    //ok
+            'municipio'                 => $municipio,
+            'bairro'                    => $linha['bairro'],                                     //ok
+            'data_inicio_atividade'     => $this->formatarData($linha['data_inicio_atividade']), //ok
+            'matriz'                    => $matriz_filial,
+            'simples'                   => ($simples['opcao_pelo_simples'] == 'S') ? 1 : 0, //ok
+            'mei'                       => ($simples['opcao_pelo_mei'] == 'S') ? 1 : 0,     //ok
+            'situacao_cadastral'        => $situacao_cadastral,
+            'data_situacao_cadastral'   => $this->formatarData($linha['data_situacao_cadastral']), //ok
             'motivo_situacao_cadastral' => $motivo_situacao_cadastral,
         ];
         $keys = array_keys($dados);
 
-        try{
-        DB::table('base')->upsert([$dados],['cnpj'],  $keys);
+        try {
+            DB::table('base')->upsert([$dados], ['cnpj'], $keys);
         } catch (Exception $e) {
 
             dd($e);exit;
@@ -111,14 +108,19 @@ class EstabelecimentoUseCase extends CsvChunkReader
 
     public function __invoke($file)
     {
-           $progress = DB::table('csv_progress')->where('filename', basename($file))->first();
+        $progress   = DB::table('csv_progress')->where('filename', basename($file))->first();
         $startChunk = $progress->last_chunk ?? 0;
+
+        $maxPlaceholders = 65535;
+        $colunas         = count($this->colunas);
+        $chunkSize       = floor($maxPlaceholders / $colunas);
         foreach ($this->readCsv($file, $this->colunas, $startChunk) as $chunk) {
-            try {
 
+            foreach (array_chunk($chunk, $chunkSize) as $chunkInsert) {
+                try {
 
-                DB::table('estabelecimento')->upsert($chunk, ['cnpj_basico'], $this->colunas);
-               // foreach ($chunk as &$linha) {
+                    DB::table('estabelecimento')->upsert($chunkInsert, ['cnpj_basico'], $this->colunas);
+                    // foreach ($chunk as &$linha) {
                     // $inicio = microtime(true);
                     // $empresa = $this->pegarEmpresa($linha['cnpj_basico']);
                     // $simples = $this->pegarSimples($linha['cnpj_basico']);
@@ -157,17 +159,18 @@ class EstabelecimentoUseCase extends CsvChunkReader
                     // $fim   = microtime(true);
                     // $tempo = $fim - $inicio;
                     // echo '✅ OK - Linha inserida com sucesso em ' . number_format($tempo, 2) . ' segundos.' . PHP_EOL;
-               // }
+                    // }
 
-            } catch (Exception $e) {
+                } catch (Exception $e) {
 
-                file_put_contents('/tmp/erro.txt', print_r($e->getMessage(), true) . PHP_EOL . print_r($chunk, true));
-                dd($e);
+                    file_put_contents('/tmp/erro.txt', print_r($e->getMessage(), true) . PHP_EOL . print_r($chunk, true));
+                    dd($e);
 
-                return false;
+                    return false;
+                }
             }
         }
-            return true;
+        return true;
 
     }
 
