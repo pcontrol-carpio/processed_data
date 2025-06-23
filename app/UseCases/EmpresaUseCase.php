3856b27313ec0e5170b7107884252f0671238d7f
Marcus Brasizza <marcus.brasizza@epoc.tech>
2025-06-23 12:02:29 -0300

    env

--- app/UseCases/EmpresaUseCase.php
+++ app/UseCases/EmpresaUseCase.php
@@ -21,7 +21,7 @@ class EmpresaUseCase extends CsvChunkReader
         $progress   = DB::table('csv_progress')->where('filename', basename($file))->first();
         $startChunk = $progress->last_chunk ?? 0;
         echo PHP_EOL;
-        echo "Iniciando processamento do arquivo: {$file}".PHP_EOL;
+        echo "Iniciando processamento do arquivo: {$file}" . PHP_EOL;
         foreach ($this->readCsv($file, $this->colunas, $startChunk) as $chunk) {
             try {
                 // Visualização: Mostra cada linha que será inserida/atualizada
@@ -39,17 +39,16 @@ class EmpresaUseCase extends CsvChunkReader
                             if (empty($value)) {
                                 $linha[$key] = (string) '';
                             }
-                        }
-                        else{
+                        } else {
                             $linha[$key] = (string) $value;
                         }
 
                     }
 
-
                 }
-                 DB::table('empresa')->upsert($chunk ['cnpj_basico'], $this->colunas);
-                    echo "Chunk inserido com sucesso".PHP_EOL;
+
+                DB::table('empresa')->upsert($chunk, ['cnpj_basico'], $this->colunas);
+                echo "Chunk inserido com sucesso" . PHP_EOL;
 
             } catch (Exception $e) {
                 echo '❌ Erro ao inserir linha:  ' . $key . PHP_EOL;
@@ -57,29 +56,25 @@ class EmpresaUseCase extends CsvChunkReader
                 file_put_contents('/tmp/erro.txt', print_r($e->getMessage(), true) . PHP_EOL);
                 exit;
 
-
-                // foreach ($chunk as $key => $linha) {
-
-
-                //     if(empty($linha['porte'])){
-                //         $linha['porte'] = null;
-
-                //     }
-                //     if(empty($linha['ente_federativo'])){
-                //         $linha['ente_federativo'] = null;
-
-                //     }
-
-
-                //     DB::table('empresa')->upsert([$linha], ['cnpj_basico'], $this->colunas);
-                //     echo "✅ {$linha['cnpj_basico']} -  {$linha['razao_social']} -  Inserido com sucesso." . PHP_EOL;
-                //     file_put_contents('/tmp/empresas.txt', "{$linha['cnpj_basico']} -  {$linha['razao_social']} -  Inserido com sucesso." . PHP_EOL,FILE_APPEND);
-
-
-                // }
             }
+            // foreach ($chunk as $key => $linha) {
 
+            //     if(empty($linha['porte'])){
+            //         $linha['porte'] = null;
+
+            //     }
+            //     if(empty($linha['ente_federativo'])){
+            //         $linha['ente_federativo'] = null;
+
+            //     }
+
+            //     DB::table('empresa')->upsert([$linha], ['cnpj_basico'], $this->colunas);
+            //     echo "✅ {$linha['cnpj_basico']} -  {$linha['razao_social']} -  Inserido com sucesso." . PHP_EOL;
+            //     file_put_contents('/tmp/empresas.txt', "{$linha['cnpj_basico']} -  {$linha['razao_social']} -  Inserido com sucesso." . PHP_EOL,FILE_APPEND);
+
+            // }
         }
+
         echo '✅ Todos os registros foram processados com sucesso.' . PHP_EOL;
         // Retorna true para indicar que o processamento foi concluído com sucesso
         echo '✅ Processamento concluído.' . PHP_EOL;
